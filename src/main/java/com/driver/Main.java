e7515652cabba3149cbed0eef16774b2e7b09a39
popsy porsanto <popsyporsanto>
2023-01-13 21:27:24 +0530

    implementation of polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,22 +2,24 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        product p=new product();
-        p.product(33,44);
-        p.product(54,62,71);
-        p.product(74.67,4989.6875);
+        Product p=new Product();
+        p.Product(33,44);
+        p.Product(54,62,71);
+        p.Product(74.67,4989.6875);
 
     }
 }
 
- class product{
-      void product(int x, int y) {
+ class Product{
+      void Product(int x, int y) {
+
           System.out.println(x+" "+y);
      }
-      void product(int x, int y, int z) {
+      void Product(int x, int y, int z) {
+
           System.out.println(x+" "+y+" "+z);
      }
-     void product(double x, double y) {
+     void Product(double x, double y) {
          System.out.println(x+" "+y);
      }
 }
