75af0930ab43ec67de1732bf430450fa2cc3a3b0
popsy porsanto <popsyporsanto>
2023-01-13 16:30:34 +0530

    polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        product p=new product();
+        p.product(33,44);
+        p.product(54,62,71);
+        p.product(74.67,4989.6875);
 
+    }
+}
+
+ class product{
+      void product(int x, int y) {
+          System.out.println(x+" "+y);
+     }
+      void product(int x, int y, int z) {
+          System.out.println(x+" "+y+" "+z);
+     }
+     void product(double x, double y) {
+         System.out.println(x+" "+y);
+     }
 }
